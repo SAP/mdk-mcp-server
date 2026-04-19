6d3d76090f94ffe08c5a84ab500222b92f088a02
thomaszang <thomas.zang@sap.com>
2026-04-19 19:35:16 +0800

    release 0.3.7

--- build/index.js
+++ build/index.js
@@ -16,7 +16,7 @@ let filenameList = [];
 let contentList = [];
 const server = new Server({
     name: "mdk-mcp",
-    version: "0.3.6",
+    version: "0.3.7",
 }, {
     capabilities: {
         tools: {},
